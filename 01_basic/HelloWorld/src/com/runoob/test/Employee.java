a350faa6e70c582789abd0ab95b1bafa4c49c4e8
kael0927 <1192045470@qq.com>
2026-05-05 13:34:21 +0800

    整理文件夹

--- 01_basic/HelloWorld/src/com/runoob/test/Employee.java
+++ 01_basic/HelloWorld/src/com/runoob/test/Employee.java
@@ -1,49 +1,27 @@
 package com.runoob.test;
-
-public class Employee {
-    private String name;
-    private int age;
-    private String designation;
+import java.io.*;
+public class Employee{
+    // 这个成员变量对子类可见
+    public String name;
+    // 私有变量，仅在该类可见
     private double salary;
-    //Employee类的构造器
-    public Employee(String name){
-        this.name = name;
+    //在构造器中对name赋值
+    public Employee (String empName){
+        name = empName;
     }
-    public void setAge(int age){
-        this.age = age;
+    //设定salary的值
+    public void setSalary(double empSal){
+        salary = empSal;
     }
-    public int getAge(){
-        return age;
-    }
-    public void setDesignation(String designation){
-        this.designation = designation;
-    }
-    public String getDesignation(){
-        return designation;
-    }
-    public void setSalary(double salary){
-        this.salary = salary;
-    }
-    public double getSalary(){
-        return salary;
-    }
-    public void printEmployee(){
-        System.out.println(this);
-    }
-    @Override
-    public String toString(){
-        return "名字：" + name + "\n" +
-                "年龄: " + age + "\n" +
-                "职位: " + designation + "\n" +
-                "薪水: " + salary;
-    }
-    public static void main(String[] args) {
-        Employee e = new Employee("李四");
-        e.setAge(22);
-        e.setDesignation("程序员");
-        e.setSalary(7500);
-        e.printEmployee();
+    // 打印信息
+    public void printEmp(){
+        System.out.println("名字 : " + name );
+        System.out.println("薪水 : " + salary);
     }
 
+    public static void main(String[] args){
+        Employee empOne = new Employee("RUNOOB");
+        empOne.setSalary(1000.0);
+        empOne.printEmp();
+    }
 }
-
--- 01_basic/HelloWorld/src/com/runoob/test/RunoobTest.java
+++ 01_basic/HelloWorld/src/com/runoob/test/RunoobTest.java
@@ -1,6 +1,8 @@
 package com.runoob.test;
 
 public class RunoobTest {
+    private int c;//私有成员变量
+    public String d = "Hello";//公有成员变量
     private int instanceVar;
     private static int staticVar;
     public void method(int paramVar){
@@ -14,7 +16,12 @@ public class RunoobTest {
         System.out.println("局部变量" + localVar);
     }
     public static void main(String[] args){
+        RunoobTest obj = new RunoobTest();//创建对象
         RunoobTest v = new RunoobTest();
+        obj.c = 10;//访问成员变量，并设置其值为10
+        System.out.println("a = " + obj.c);
+        obj.d = "world";
+        System.out.println("b = " + obj.d);
         v.method(20);
         int a = 10,b = 20;
         swap(a,b);
